d99186a2738c54b0c95a424f7ebeaef7df65c1a7
shamsher <shamsher.kc.c4@gmail.com>
2024-04-21 15:07:52 +0900

    project commited

---
+++ js/main.js
@@ -0,0 +1,73 @@
+document.getElementById("change-theme").addEventListener("click", changeTheme);
+
+function changeTheme()
+{
+  var checkbox = document.getElementById('change-theme');
+  if (checkbox.checked == true)
+  {
+        console.log("theme : blue" + ` and it's boolean value is = ${checkbox.checked}`);
+        document.querySelector("h1").children[0].style.color = "rgb(12, 151, 174)";
+        document.getElementsByClassName("personal-info-padder")[0].children[0].children[0].style.color = "rgb(12, 151, 174)";
+        document.getElementsByClassName("personal-column")[0].children[0].style.color = "rgb(12, 151, 174)";
+        document.getElementsByClassName("top-banner")[0].children[0].parentElement.classList.add("bluebanner");
+        document.getElementsByClassName("top-banner")[0].children[0].parentElement.classList.remove("orangebanner");
+        document.getElementsByClassName("top-banner")[0].children[0].parentElement.classList.remove("bluebanner");
+        document.getElementsByClassName("social-media")[0].children[0].children[0].style.color = "rgb(12, 151, 174)";
+        document.getElementsByClassName("social-media")[0].children[1].children[0].style.color = "rgb(12, 151, 174)";
+        document.getElementsByClassName("social-media")[0].children[2].children[0].style.color = "rgb(12, 151, 174)";
+        document.getElementById("self-img").src = "shamsherkc.jpeg";
+        document.getElementsByClassName("footer-section")[0].style.backgroundColor = "rgb(12, 151, 174)";
+        document.getElementsByClassName("footer-section")[0].style.transition = "0.5s";
+        
+
+
+  } else
+
+    {
+        console.log("theme : orange" + ` and it's boolean value is = ${checkbox.checked}`);
+        document.querySelector("h1").children[0].style.color = "rgb(187, 105, 18)";
+        document.getElementsByClassName("personal-info-padder")[0].children[0].children[0].style.color = "rgb(187, 105, 18)";
+        document.getElementsByClassName("personal-column")[0].children[0].style.color = "rgb(187, 105, 18)";
+        document.getElementsByClassName("top-banner")[0].children[0].parentElement.classList.add("orangebanner");
+        document.getElementsByClassName("top-banner")[0].children[0].parentElement.classList.remove("bluebanner");
+        document.getElementsByClassName("social-media")[0].children[0].children[0].style.color = "rgb(187, 105, 18)";
+        document.getElementsByClassName("social-media")[0].children[1].children[0].style.color = "rgb(187, 105, 18)";
+        document.getElementsByClassName("social-media")[0].children[2].children[0].style.color = "rgb(187, 105, 18)";
+        document.getElementById("self-img").src = "selfimg.png";
+        document.getElementsByClassName("footer-section")[0].style.backgroundColor = "rgb(187, 105, 18)";
+        document.getElementsByClassName("footer-section")[0].style.transition = "0.5s";
+        document.getElementById('self-img').style.transition = "0.5s";
+
+
+
+    
+   
+   //for main button contactme and ask CV
+        // let mainBtnLength = document.getElementsByClassName("main-button");
+        // for (i = 0; i<=mainBtnLength.length; i++) 
+        // {
+        //     document.getElementsByClassName("main-button")[i].style.backgroundColor = "rgb(187, 105, 18)";
+        // }
+
+        //h3 style (my story and personal information)
+       
+    }
+ 
+
+}
+//let chk = document.getElementsByClassName('change-theme');
+
+let bool =  (num1, num2) =>  num1 + num2
+
+
+console.log(bool(2,4));
+
+
+
+
+
+
+
+
+
+
